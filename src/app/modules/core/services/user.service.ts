f2d726da76c9f095e2610fe3416e658cd16d37c3
Reem Ben-David <reemile1996@gmail.com>
2024-07-23 11:49:40 +0300

    Task 6 - Add error handling

--- src/app/modules/core/services/user.service.ts
+++ src/app/modules/core/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, map, take, tap } from 'rxjs/operators';
 import { HttpService } from './http.service';
 import { User } from 'app/modules/features/user/models/user.model';
@@ -23,23 +23,25 @@ export class UserService {
     );
   }
 
-  public updateUser(uid: string, changes: Partial<User>) {
+  public updateUser(userId: string, changes: Partial<User>) {
     return this.users$.pipe(
       take(1),
       map((users: User[]) => {
-        const oldUser = users.find((user) => user.id === uid);
+        const oldUser = users.find((user) => user.id === userId);
         const updatedUser: User = { ...oldUser, ...changes };
-        const updatedUsers: User[] = users.map((user) => (user.id === uid ? updatedUser : user));
+        const updatedUsers: User[] = users.map((user) => (user.id === userId ? updatedUser : user));
 
         this.usersSubject.next(updatedUsers);
         return updatedUser;
-      })
+      }),
+      catchError(this.handleError<User>('updateUser'))
     );
   }
   private handleError<T>(operation = 'operation') {
     return (error: Error): Observable<T> => {
-      this.logger.error(`${operation} failed: ${error.message}`);
-      return throwError(() => new Error('Failed to fetch users'));
+      const message = `${operation} failed: ${error.message}`;
+      this.logger.error(message);
+      return throwError(() => new Error(message));
     };
   }
 }
